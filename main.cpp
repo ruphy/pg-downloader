b3cefd048f148a4417fc5748a5d912fbd4b92bc4
Riccardo Iaconelli <riccardo@kde.org>
2008-10-21 13:12:56 +0200

    udpate
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- main.cpp
+++ main.cpp
@@ -3,44 +3,36 @@
 #include <QtGui>
 #include <QtNetwork>
 
-class Parser : public QObject
+
+class Item
+{
+  public:
+    QString name, cap, locality, region, address, tel;
+}
+
+class Parser : public QXmlStreamReader
 {
   Q_OBJECT
   public:
-    void setUrl(QString url) { m_url = url; }
-    void parse();
-
-  public slots:
-    void processData(QNetworkReply*);
+    void parse(QIODevice*);
 
   private:
-    QString m_url;
-    QString m_params;
-//     QHttp *m_http;
 };
 
-void Parser::parse()
+void Parser::parse(QIODevice *d)
 {
-  int page = 0;
-  m_params = "p-"+QString(page)+"?mr=100&rk=4";
-  QString finalUrl = m_url+m_params;
-  QNetworkAccessManager *manager = new QNetworkAccessManager(this);
-  connect(manager, SIGNAL(finished(QNetworkReply*)),
-          this, SLOT(processData(QNetworkReply*)));
+  setDevice(d);
+  while (!atEnd()) {
+    readNext();
 
-  manager->get(QNetworkRequest(QUrl(finalUrl)));
-}
-
-void Parser::processData(QNetworkReply *reply)
-{
-
+  }
 }
 
 int main(int argc, char** argv)
 {
   QApplication a(argc, argv);
   Parser p;
-  p.setUrl("http://www.paginegialle.it/pgol/1-006644400/4-ottica%20lenti%20a%20contatto%20ed%20occhiali%20vendita%20al%20dettaglio/l-1/");
+  p.parse(QFile("big-bad-file.html");
 
   a.exec();
 }
