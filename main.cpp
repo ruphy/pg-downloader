b7ccfe578e61e16724103f7c804dda34d2302bdc
Riccardo Iaconelli <riccardo@kde.org>
2008-10-26 21:48:42 +0100

    update
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- main.cpp
+++ main.cpp
@@ -14,24 +14,67 @@ class Parser : public QXmlStreamReader
 {
   public:
     void parse(QIODevice*);
+  private:
+    void readClient();
+
+    QList<Item*> items;
 };
 
 void Parser::parse(QIODevice *d)
 {
-  setDevice(d);
-  while (!atEnd()) {
-    readNext();
+    setDevice(d);
+    
+    while (!atEnd()) {
+        
+        readNext();
+        if (hasError()) {
+            qDebug() << "ERROR!!!";
+        }
+        
+    //     if (isStartElement() && name() == "div" && attributes().value("class") == "client-identifying-pg") {
 
-  }
+    //       qDebug() << text().toString().toUtf8();
+    //       while ((!isEndElement()) && name() == "div" && attributes().value("class") == "client-identifying-pg") {
+    // 	readNext();
+
+            if (isStartElement() && name() == "h3" && attributes().value("class") == "org") { // Process informations for one company
+            Item *i = new Item;
+            while (i->name.isEmpty()) {
+                readNext();
+                if (tokenType() == QXmlStreamReader::Characters) {
+                    i->name = text().toString();
+                }
+            }
+        items << i;
+        }
+    }
+    
+    foreach(Item *item, items) {
+        qDebug() << item->name;
+    }
+}
+
+void Parser::readClient()
+{
+//   forever {
+
+
+
+//   }
 }
 
 int main(int argc, char** argv)
 {
   QApplication a(argc, argv);
   Parser p;
-  p.parse(new QFile("big-bad-file.html"));
+  QFile *f = new QFile("test.html");
+//   qDebug() << f->exists();
+  f->open(QIODevice::ReadOnly);
+  p.parse(f);
+  f->close();
+  
 
-  a.exec();
+//   a.exec();
 }
 
 
