aaf9973d9e7958864b23bc04e0e9c9632c569b10
Dario Freddi <drf54321@gmail.com>
2008-10-26 22:30:59 +0100

    Everything's cool, except phone number
    Signed-off-by: Dario Freddi <drf54321@gmail.com>

--- main.cpp
+++ main.cpp
@@ -56,7 +56,7 @@ void Parser::parse(QIODevice *d)
         }
 
         forever {
-            if (isStartElement() && name() == "span" && attributes().value("class") == "postal-code") { // Process informations for one company
+            if (isStartElement() && name() == "span" && attributes().value("class") == "postal-code") {
                 while (i->cap.isEmpty()) {
                     readNext();
                     if (tokenType() == QXmlStreamReader::Characters) {
@@ -71,11 +71,77 @@ void Parser::parse(QIODevice *d)
             readNext();
         }
 
+        forever {
+            if (isStartElement() && name() == "span" && attributes().value("class") == "locality") {
+                while (i->locality.isEmpty()) {
+                    readNext();
+                    if (tokenType() == QXmlStreamReader::Characters) {
+                        i->locality = text().toString();
+                    }
+                }
+                break;
+            }
+            if (atEnd()) {
+                break;
+            }
+            readNext();
+        }
+
+        forever {
+            if (isStartElement() && name() == "span" && attributes().value("class") == "region") {
+                while (i->region.isEmpty()) {
+                    readNext();
+                    if (tokenType() == QXmlStreamReader::Characters) {
+                        i->region = text().toString();
+                        i->region.remove(')');
+                        i->region.remove('(');
+                    }
+                }
+                break;
+            }
+            if (atEnd()) {
+                break;
+            }
+            readNext();
+        }
+
+        forever {
+            if (isStartElement() && name() == "p" && attributes().value("class") == "street-address") {
+                while (i->address.isEmpty()) {
+                    readNext();
+                    if (tokenType() == QXmlStreamReader::Characters) {
+                        i->address = text().toString();
+                    }
+                }
+                break;
+            }
+            if (atEnd()) {
+                break;
+            }
+            readNext();
+        }
+
+        forever {
+            if (isStartElement() && name() == "p" && attributes().value("class") == "tel") {
+                while (i->tel.isEmpty()) {
+                    readNext();
+                    if (tokenType() == QXmlStreamReader::Characters) {
+                        i->tel = text().toString();
+                    }
+                }
+                break;
+            }
+            if (atEnd()) {
+                break;
+            }
+            readNext();
+        }
+
         items << i;
     }
 
     foreach(Item *item, items) {
-        qDebug() << item->name << item->cap;
+        qDebug() << item->name << item->cap << item->locality << item->region << item->address << item->tel;
     }
 }
 
